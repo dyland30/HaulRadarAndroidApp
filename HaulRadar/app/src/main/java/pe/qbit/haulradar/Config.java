593849fcf90869dc84533f281ffe04da002d1736
Castillo <dcastillo@lima-airport.com>
2016-10-19 18:00:53 -0500

    version 1

---
+++ HaulRadar/app/src/main/java/pe/qbit/haulradar/Config.java
@@ -0,0 +1,15 @@
+package pe.qbit.haulradar;
+
+/**
+ * Created by dcastillo on 19/10/2016.
+ */
+
+public class Config {
+    public static String PROTOCOL = "http://";
+    public static String SERVER = "104.198.226.229";
+
+
+    public static String USER_LOGIN_METHOD = "/user/login/:email/:clave";
+
+
+}
